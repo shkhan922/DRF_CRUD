eb95bc61a6cb5f851f7f52a9f9170ce3e00b77b6
shkhan922 <shkhan922@gmail.com>
2024-01-31 20:44:11 +0530

    Tracks can be add with and no new tracks created playlist creation

--- drf_crud/app/serializers.py
+++ drf_crud/app/serializers.py
@@ -5,20 +5,19 @@ from .models import Track, Playlist, PlaylistTrack
 class TrackSerializer(serializers.ModelSerializer):
     class Meta:
         model = Track
-        fields = '__all__'
+        fields = ['id', 'name', 'artist_name', 'album_name']
 
-# class PlaylistSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Playlist
-#         fields = '__all__'
+class PlaylistTrackSerializer(serializers.ModelSerializer):
+    track_id = serializers.IntegerField(source='track.id')
+    track_name = serializers.CharField(source='track.name')
+
+    class Meta:
+        model = PlaylistTrack
+        fields = ['track_id', 'track_name', 'sequence_number']
+
 class PlaylistSerializer(serializers.ModelSerializer):
-    tracks = TrackSerializer(many=True, read_only=True)
+    tracks = PlaylistTrackSerializer(many=True, read_only=True, source='playlisttrack_set')
 
     class Meta:
         model = Playlist
         fields = ['id', 'name', 'playlist_id', 'tracks']
-
-class PlaylistTrackSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = PlaylistTrack
-        fields = '__all__'
